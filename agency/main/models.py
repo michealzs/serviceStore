b883e5fc1433e53b7cffc637e36ef65a75b4ed6d
michealzs <seuntuyie@gmail.com>
2024-03-24 13:21:37 +0000

    update

--- agency/main/models.py
+++ agency/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class HomeModel(models.Model):
+    title = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
+    text = models.text()
+    image = models.ImageField(upload_to='static/images/')
+
+    def __str__(self):
+        return self.title
--- agency/main/views.py
+++ agency/main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, TemplateView, DeleteView
+
+
+class HomeView(ListView):
+    model = HomeModel
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(50*'8', '\n', self.request.META.get('HTTP_X_REAL_IP'), "Just Visited US", '\n', 50*'8')
+        if self.request.user.is_authenticated:
+            print('pure user ==>', self.request.user)
+            print('pure username ==>', self.request.user.username)
+            #print('pure email ==>', self.request.user.email)
+            #print(50*'#', 'Session ===>', self.request.session)
+            #print('pure META ==>', self.request.META)
+            #print('pure Visitor ID ==>', self.request.session['visitor_id'])
+        else:
+            print("<== User is not Authenticated ==>")
+        context['featured_products'] = Product.objects.featured()
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        sessione(self.request)
+        return super().dispatch(request, *args, **kwargs)
