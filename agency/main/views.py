fc3423709c4f42bf6a03f39f774062cb260d10b8
micheal <147457347+michealzs@users.noreply.github.com>
2024-04-02 19:44:26 -0400

    updates

--- agency/main/views.py
+++ agency/main/views.py
@@ -26,20 +26,36 @@ class HomeView(ListView):
         return super().dispatch(request, *args, **kwargs)
     """
 
-class AboutView(ListView):
-    model = AboutModel
+class AboutView(TemplateView):
     template_name = 'about-us.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(50*'8', '\n', self.request.META.get('HTTP_X_REAL_IP'), "Just Visited US", '\n', 50*'8')
-        if self.request.user.is_authenticated:
-            print('pure user ==>', self.request.user)
-            print('pure username ==>', self.request.user.username)
-            #print('pure email ==>', self.request.user.email)
-            #print(50*'#', 'Session ===>', self.request.session)
-            #print('pure META ==>', self.request.META)
-            #print('pure Visitor ID ==>', self.request.session['visitor_id'])
-        else:
-            print("<== User is not Authenticated ==>")
-        return context
+class FaqView(TemplateView):
+    template_name = 'faq.html'
+
+
+class GrapDesignView(TemplateView):
+    template_name = 'graphic-design.html'
+
+
+class WebDesignView(TemplateView):
+    template_name = 'web-design.html'
+
+
+class MobDesignView(TemplateView):
+    template_name = 'mobile-design.html'
+
+
+class MarkettingView(TemplateView):
+    template_name = 'digital-marketting.html'
+
+
+class AppSupportView(TemplateView):
+    template_name = 'app-support.html'
+
+class WebRadioView(TemplateView):
+    template_name = 'web-radio.html'
+
+
+class JobOpeningView(TemplateView):
+    template_name = 'openings.html'
+
